66c015c6135fedff483f08fa1079fb548efbdb60
srikanthpragada <srikanthpragada@gmail.com>
2019-12-19 19:55:54 +0530

    19-DEC

--- hr/urls.py
+++ hr/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('index/', views.index)
+    path('index/', views.index),
+    path("countries/", views.list_countries),
+    path("country_info/", views.country_info)
 ]
--- hr/views.py
+++ hr/views.py
@@ -1,8 +1,34 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import requests
 
 
 # Create your views here.
 
 def index(request):
-    return render(request,'index.html')
+    return render(request, 'index.html')
+
+
+def list_countries(request):
+    resp = requests.get("https://restcountries.eu/rest/v2/all")
+    if resp.status_code == 200:
+        countries = sorted(resp.json(),
+                           key = lambda c : c['population'],
+                           reverse=True)
+        return render(request, 'list_countries.html',
+                      {'countries': countries})
+    else:
+        return HttpResponse("<h2>Sorry! Could not get details of countries!</h1>")
+
+
+def country_info(request):
+    if 'code' not in request.GET:
+        return  HttpResponse("<h2>Sorry! Missing country code!</h2>")
+
+    code = request.GET['code']   # code passed in URL
+    resp = requests.get(f"https://restcountries.eu/rest/v2/alpha/{code}")
+    if resp.status_code == 200:
+        country = resp.json()
+        return render(request, 'country_info.html',{'country': country})
+    else:
+        return HttpResponse("<h2>Sorry! Could not find country code!</h2>")
